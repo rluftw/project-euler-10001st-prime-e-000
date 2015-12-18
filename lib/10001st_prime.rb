b61f857ccf0c58f44ff8e10168c101faa9231d18
rluftw <richuuuurd@gmail.com>
2015-12-17 21:04:46 -0500

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,31 @@
 # Implement your procedural solution here!
+
+def prime_number_for(number)
+	prime_numbers = []
+	count = 1
+
+	until prime_numbers.length == number
+		if is_prime(count)
+			prime_numbers << count
+		end
+		count += 1
+	end
+	prime_numbers[number-1]
+end
+
+def is_prime(number) 
+	if number < 2
+		false
+	else 	
+		multiples = []
+
+		Math.sqrt(number).floor.downto(1) do |num| 
+			if number % num == 0 
+				multiples << num
+			end	
+		end
+
+		multiples.length == 1
+	end
+end
+
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,37 @@
 # Implement your object-oriented solution here!
+
+class Prime
+	def initialize(num)
+		@num = num
+	end
+
+	def number
+		prime_numbers = []
+		count = 1
+
+		until prime_numbers.length == @num
+			if is_prime(count)
+				prime_numbers << count
+			end
+			count += 1
+		end
+		prime_numbers[@num-1]
+	end
+
+	def is_prime(number) 
+		if number < 2
+			false
+		else 	
+			multiples = []
+
+			Math.sqrt(number).floor.downto(1) do |num| 
+				if number % num == 0 
+					multiples << num
+				end	
+			end
+
+			multiples.length == 1
+		end
+	end
+end
+
